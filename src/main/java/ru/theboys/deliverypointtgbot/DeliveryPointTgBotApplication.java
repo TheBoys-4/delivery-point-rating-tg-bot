bd4274e312635cfe727adad8f5621b877cb70517
Daniil Paveliev <khiromuelrick@gmail.com>
2023-05-23 00:06:57 +0300

    init commit

---
+++ src/main/java/ru/theboys/deliverypointtgbot/DeliveryPointTgBotApplication.java
@@ -0,0 +1,13 @@
+package ru.theboys.deliverypointtgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryPointTgBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryPointTgBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/theboys/deliverypointtgbot/DeliveryPointTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.theboys.deliverypointtgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryPointTgBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
