d8b394ddaded83e406ef4374ec9c15ffe80eda4f
Daniil Paveliev <khiromuelrick@gmail.com>
2023-05-28 21:26:56 +0300

    push

--- src/test/java/ru/theboys/deliverypointratingtgbot/DeliveryPointRatingTgBotApplicationTests.java
+++ src/test/java/ru/theboys/deliverypointratingtgbot/DeliveryPointRatingTgBotApplicationTests.java
@@ -1,13 +1,1 @@
 package ru.theboys.deliverypointratingtgbot;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DeliveryPointRatingTgBotApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
