6e5eb6e2576f3d6233175e2187c103137908750f
Daniil Paveliev <khiromuelrick@gmail.com>
2023-05-28 22:39:16 +0300

    push

--- src/main/java/ru/theboys/deliverypointratingtgbot/constants/TextConstants.java
+++ src/main/java/ru/theboys/deliverypointratingtgbot/constants/TextConstants.java
@@ -3,7 +3,7 @@ package ru.theboys.deliverypointratingtgbot.constants;
 import javax.xml.transform.sax.SAXResult;
 
 public class TextConstants {
-    public static final String ABOUT_ME_MESSAGE = "Ноль, целковый, ватрушка.";
+    public static final String ABOUT_ME_MESSAGE = "В данном чате можно оставить отзыв. Нажмите \"Оставить комментарии\". Затем введите номер вашего заказа. После чего введите оценку и добавте к нему сообщение. Спасибо за ваш отзыв и за то, что помогаете нам становить лучше!";
     public static final String GIVE_ORDER_ID_MESSAGE = "Введите номер заказа";
     public static final String GIVE_COMMENT_MESSAGE = "Напишите ваш комментарий";
     public static final String ERROR_MESSAGE = "Something wrong";
